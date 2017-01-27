de35232d5d918c6a3c61ee0c95b68162facffb64
William W Wold <wilcoe@rocketmail.com>
2017-01-27 00:02:33 -0800

    implemented blur

--- Window.cpp
+++ Window.cpp
@@ -142,8 +142,8 @@ void Window::setupTexture()
 	glBindTexture(GL_TEXTURE_2D, texture);
 	
 	// Set the texture wrapping/filtering options (on the currently bound texture object)
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_MIRRORED_REPEAT);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_MIRRORED_REPEAT);
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
 	
--- shader0.frag
+++ shader0.frag
@@ -7,8 +7,22 @@ out vec4 color;
 
 uniform sampler2D ourTexture;
 
+const float blurSize=0.04;
+const int blurIter=12;
+
 void main()
 {
-    color = texture(ourTexture, TexCoord);
+	vec3 clrAvg=vec3(0, 0, 0);
+	
+	for (int x=0; x<blurIter; x++)
+	{
+		for (int y=0; y<blurIter; y++)
+		{
+			clrAvg+=texture(ourTexture, vec2(TexCoord.x+float(x)*blurSize/blurIter, TexCoord.y+float(y)*blurSize/blurIter)).rgb;
+		}
+	}
+	
+	//color = texture(ourTexture, TexCoord);
+	color=vec4(clrAvg/(blurIter*blurIter), 1.0f);
 }
 
